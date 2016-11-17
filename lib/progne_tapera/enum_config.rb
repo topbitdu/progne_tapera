962fc0c845aac417862a61895fb1a5c054970f1f
Topbit Du <topbit.du@gmail.com>
2016-11-17 09:06:24 +0800

    1, Improve the Enum Config concern to support the customized enum i18n name.

--- lib/progne_tapera/enum_config.rb
+++ lib/progne_tapera/enum_config.rb
@@ -8,14 +8,14 @@ module ProgneTapera::EnumConfig
 
   module ClassMethods
 
-    def enum(name = nil)
+    def enum(name = nil, localized_name = name)
       name = enum_name(name).to_s
       enumerations = Rails.configuration.enum[name]
       raise ArgumentError.new "The enum.#{name} was not configured in the config/enum.yml file." if enumerations.blank?
       enumerations.each do |key, value|
         options = value.map { |k, v| [ k.to_sym, v ] }.to_h
         code    = options.delete :code
-        options[:localized_name] = I18n.t "enum.#{name}.#{key}"
+        options[:localized_name] = I18n.t "enum.#{localized_name}.#{key}"
         safe_add_item ProgneTapera::EnumItem.new(code, key, options)
       end
     end
