39bfc50c4eacc9fa050c55537aa3b5f5dd3294f2
Topbit Du <topbit.du@gmail.com>
2016-11-20 11:09:04 +0800

    1, Improve the Enum Config concern to add the .overload_enum_i18n method. 2, Improve the Enum Config concern to handle the empty name argument for the i18n.

--- lib/progne_tapera/enum_config.rb
+++ lib/progne_tapera/enum_config.rb
@@ -9,15 +9,26 @@ module ProgneTapera::EnumConfig
   module ClassMethods
 
     def enum(name = nil, localized_name = name)
-      name = enum_name(name).to_s
+
+      name         = enum_name(name).to_s
       enumerations = Rails.configuration.enum[name]
-      raise ArgumentError.new "The enum.#{name} was not configured in the config/enum.yml file." if enumerations.blank?
+      raise ArgumentError.new("The enum.#{name} was not configured in the config/enum.yml file.") if enumerations.blank?
+
       enumerations.each do |key, value|
         options = value.map { |k, v| [ k.to_sym, v ] }.to_h
         code    = options.delete :code
-        options[:localized_name] = I18n.t "enum.#{localized_name}.#{key}"
+        options[:localized_name] = I18n.t "enum.#{localized_name||name}.#{key}"
         safe_add_item ProgneTapera::EnumItem.new(code, key, options)
       end
+
+    end
+
+    def overload_enum_i18n(localized_name = nil)
+
+      each do |enum_item|
+        enum_item.options[:localized_name] = I18n.t "enum.#{localized_name}.#{enum_item.name}"
+      end
+
     end
 
   end
